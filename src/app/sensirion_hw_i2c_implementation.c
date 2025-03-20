300ce07d16f91b0700e5ec914c5bbfab711a6129
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2025-03-20 11:31:14 -0600

    implemented sensirion sleep

--- src/app/sensirion_hw_i2c_implementation.c
+++ src/app/sensirion_hw_i2c_implementation.c
@@ -35,6 +35,7 @@
 #include "drivers/i2c_driver.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 /*
  * INSTRUCTIONS
@@ -174,5 +175,9 @@ int8_t sensirion_i2c_write(uint8_t address, const uint8_t *data, uint16_t count)
  */
 void sensirion_sleep_usec(uint32_t useconds)
 {
-    // IMPLEMENT
+    struct timespec req;
+    req.tv_sec = useconds / 1000000;            // Convert microseconds to seconds
+    req.tv_nsec = (useconds % 1000000) * 1000;  // Convert remainder to nanoseconds
+
+    clock_nanosleep(CLOCK_MONOTONIC, 0, &req, NULL);
 }
