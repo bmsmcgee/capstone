b26f71f0d496fabed41d5f762be35036a66fe5c2
bmsmcgee <bmsmcgee@outlook.com>
2025-03-27 23:20:41 -0600

    updated for sht4x

--- src/app/app_main.c
+++ src/app/app_main.c
@@ -24,8 +24,7 @@
 
 LOG_LEVEL_INIT(LOG_LEVEL_INFO);
 
-// static void send_sht4x_data(void);
-// static void send_bq35100_data(void);
+static void send_sht4x_data(void);
 
 int app_main(int argc, const char *argv[])
 {
@@ -36,52 +35,43 @@ int app_main(int argc, const char *argv[])
     // rodeo_init("demo");
 
     INFO("%s", "Initializing I2C communication...");
-    // sensirion_i2c_init();
-    sht45_init();
 
-    // INFO("%s", "Triggering BQ35100 fuel gauge to start...");
-    // if (bq35100_send_subcommand(BQ35100_SUB_GAUGE_START) != 0)
-    // {
-    //     ERR("%s", "Failed to trigger BQ35100 into ACTIVE mode.");
-    //     return -1;
-    // }
+    sht45_init();
 
     INFO("%s", "Sensors detected. Starting data collection...");
 
-    // while (1)
-    // {
-    //     send_sht4x_data();
-    //     // sleep(1);
-    //     // send_bq35100_data();
-    //     sleep(5);
-    // }
+    while (1)
+    {
+        send_sht4x_data();
+        // sleep(1);
+        // send_bq35100_data();
+        sleep(5);
+    }
 
     return 0;
 }
-/*
- * EVERY FUNCTION AFTER THIS POINT WILL EVENTUALLY BE CONSOLIDATED INTO ONE FUNCTION TO SEND ALL VARIABLES TO CLOUD THROUGH MQTT
- */
-// static void send_sht4x_data(void)
-// {
-//     int32_t sht_temp;
-//     int32_t sht_hum;
 
-//     if (sht4x_measure_blocking_read(&sht_temp, &sht_hum) == 0)
-//     {
-//         float temp_cel = (float)sht_temp / 1000.0;
-//         float hum_percent = (float)sht_hum / 1000.0;
 
-//         INFO("SHT4x Sensor Data: Temperature: %.2f Celsius, Humidity: %.2f%%", temp_cel, hum_percent);
+static void send_sht4x_data(void)
+{
+    uint16_t raw_temp;
+    uint16_t raw_hum;
+    float temp_cel;
+    float hum_percent;
 
-//         float sht_data[2] = {temp_cel, hum_percent};
+    if (sht4x_read(&raw_temp, &temp_cel, &raw_hum, &hum_percent) == 0)
+    {
+        INFO("SHT4x Sensor Data: Temperature: %.2f Celsius, Humidity: %.2f%%", temp_cel, hum_percent);
 
-//         if (send_sensor_msg_float_array_by_id(SENSOR_ID_ENV, sht_data, 2) != 0)
-//         {
-//             ERR("%s", "Failed to send SHT4x sensor data!");
-//         }
-//     }
-//     else
-//     {
-//         ERR("%s", "SHT4x measurement failed!");
-//     }
-// }
+        float sht_data[2] = {temp_cel, hum_percent};
+
+        if (send_sensor_msg_float_array_by_id(SENSOR_ID_ENV, sht_data, 2) != 0)
+        {
+            ERR("%s", "Failed to send SHT4x sensor data!");
+        }
+    }
+    else
+    {
+        ERR("%s", "SHT4x measurement failed!");
+    }
+}
