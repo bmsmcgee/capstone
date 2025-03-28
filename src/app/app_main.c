096d0647c6661f2dab8c299abad628c3f8638bdb
bmsmcgee <bmsmcgee@outlook.com>
2025-03-27 23:47:18 -0600

    formatting

--- src/app/app_main.c
+++ src/app/app_main.c
@@ -12,9 +12,6 @@
 #include "rodeo.h"
 #include "sensor_message.h"
 #include "driver_sht4x.h"
-// #include "sht4x/sht4x.h"
-// #include "bq35100/bq35100.h"
-// #include "sensirion_i2c.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -26,8 +23,7 @@ LOG_LEVEL_INIT(LOG_LEVEL_INFO);
 
 static void send_sht4x_data(void);
 
-int app_main(int argc, const char *argv[])
-{
+int app_main(int argc, const char *argv[]) {
     (void)argc;
     (void)argv;
 
@@ -40,11 +36,8 @@ int app_main(int argc, const char *argv[])
 
     INFO("%s", "Sensors detected. Starting data collection...");
 
-    while (1)
-    {
+    while (1){
         send_sht4x_data();
-        // sleep(1);
-        // send_bq35100_data();
         sleep(5);
     }
 
@@ -52,26 +45,21 @@ int app_main(int argc, const char *argv[])
 }
 
 
-static void send_sht4x_data(void)
-{
+static void send_sht4x_data(void) {
     uint16_t raw_temp;
     uint16_t raw_hum;
     float temp_cel;
     float hum_percent;
 
-    if (sht4x_read(&raw_temp, &temp_cel, &raw_hum, &hum_percent) == 0)
-    {
+    if (sht4x_read(&raw_temp, &temp_cel, &raw_hum, &hum_percent) == 0) {
         INFO("SHT4x Sensor Data: Temperature: %.2f Celsius, Humidity: %.2f%%", temp_cel, hum_percent);
 
         float sht_data[2] = {temp_cel, hum_percent};
 
-        if (send_sensor_msg_float_array_by_id(SENSOR_ID_ENV, sht_data, 2) != 0)
-        {
+        if (send_sensor_msg_float_array_by_id(SENSOR_ID_ENV, sht_data, 2) != 0) {
             ERR("%s", "Failed to send SHT4x sensor data!");
         }
-    }
-    else
-    {
+    } else {
         ERR("%s", "SHT4x measurement failed!");
     }
 }
